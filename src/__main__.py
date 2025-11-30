05d5165b96edb03f455cd34eb999d52b8cf682f9
hikyae <h1ky43@gmail.com>
2025-11-30 20:05:23 +0900

    fix: lower input text for case insensitive search

--- src/__main__.py
+++ src/__main__.py
@@ -217,7 +217,7 @@ class Launcher(Gtk.Application):
             self.suggestion_box.remove(child)
             child = next_child
 
-        matches = [cmd for cmd in self.commands if subseqmatch(text, cmd.lower())]
+        matches = [c for c in self.commands if subseqmatch(text.lower(), c.lower())]
 
         if not matches:
             self.hide_suggestions()
